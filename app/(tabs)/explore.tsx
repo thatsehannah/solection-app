2cc1b18ffa5e388a8aa589c189c6aa1cf0cc6c67
E. Hannah <echannah631@gmail.com>
2025-12-09 20:19:41 -0500

    adding initial color palette

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabTwoScreen() {
   return (
-    <View>
-      <Text className='text-4xl text-blue-800 font-bold'>Explore</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-screen'>
+      <Text className='text-2xl text-primary-foreground border-2 border-secondary text-center rounded-md p-3'>
+        Explore Screen
+      </Text>
+    </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4c4b3b",
+        secondary: "#f76506",
+        "primary-foreground": "#d6cfbe",
+      },
+    },
   },
   plugins: [],
 };
